d60532150820eb5895d3634c325b008848c0dfbf
ephf <76856042+ephf@users.noreply.github.com>
2023-07-31 01:43:47 -0400

    Update va_expand.h

--- va_expand.h
+++ va_expand.h
@@ -6,11 +6,14 @@
 #define __EVAL2__(...) __EVAL3__(__EVAL3__(__EVAL3__(__EVAL3__(__EVAL3__(__VA_ARGS__)))))
 #define __EVAL3__(...) __VA_ARGS__
 
-#define __EMPTY__()
+#define __EMPTY__(...)
 #define __DEFER__(id) id __EMPTY__()
 
-#define __VA_ENDING_FALSE__(a, b, c, ...) a(c) __DEFER__(__VA_EXPAND_IND__)()(a, b, __VA_ARGS__)
-#define __VA_ENDING_TRUE__(a, b, c, ...) b(c)
+#define __VA_GRAB_A__(a, ...) a
+#define __VA_GRAB_B__(a, b, ...) b
+
+#define __VA_ENDING_FALSE__(a, b, c, ...) __VA_GRAB_A__ a(c, __VA_GRAB_B__ b) __DEFER__(__VA_EXPAND_IND__)()(a, (__VA_GRAB_A__ b, __VA_GRAB_A__ b(__VA_GRAB_B__ b)), __VA_ARGS__)
+#define __VA_ENDING_TRUE__(a, b, c, ...) __VA_GRAB_B__ a(c, __VA_GRAB_B__ b)
 #define __VA_ENDING__(a, b, ...) __VA_ENDING_ ## b
 #define __VA_ENDING_INT__(...) __VA_ENDING__(__VA_ARGS__, FALSE__)
 
@@ -19,6 +22,6 @@
 
 #define __VA_EXPAND_INT__(...) __VA_CHECK_END__(__VA_ARGS__, TRUE__)
 #define __VA_EXPAND_IND__() __VA_EXPAND_INT__
-#define __VA_EXPAND__(macro, macro_last, ...) __EVAL__(__VA_EXPAND_INT__(macro, macro_last, __VA_ARGS__))
+#define __VA_EXPAND__(macros, state, ...) __EVAL__(__VA_EXPAND_INT__(macros, state, __VA_ARGS__))
 
 #endif
